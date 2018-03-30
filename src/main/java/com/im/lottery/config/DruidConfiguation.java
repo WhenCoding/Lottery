404c745c8b827586beffa8a83cc69010700594e8
xgs <mynamecoder@163.com>
2018-03-30 10:23:06 +0800

    udpate

--- src/main/java/com/im/lottery/config/DruidConfiguation.java
+++ src/main/java/com/im/lottery/config/DruidConfiguation.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
--- src/test/java/com/im/lottery/LotteryApplicationTests.java
+++ src/test/java/com/im/lottery/LotteryApplicationTests.java
@@ -1,23 +1,15 @@
 package com.im.lottery;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.im.lottery.entity.Prize;
-import com.im.lottery.entity.Record;
-import com.im.lottery.persistence.PrizeMapper;
-import com.im.lottery.service.PrizeService;
-
+ 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = LotteryApplication.class)
+@SpringBootTest(classes = LotteryApplication.class)
 @WebAppConfiguration
 public class LotteryApplicationTests {
 	
